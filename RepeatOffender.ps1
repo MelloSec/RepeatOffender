de011b0a3d160c10af158fa10f66863330d02f8b
mellonaut <rleeazw@outlook.com>
2023-12-12 05:36:46 -0500

    winword

--- RepeatOffender.ps1
+++ RepeatOffender.ps1
@@ -18,7 +18,7 @@ $env:TMP = "C:\Temp"
 $Downloads = Get-ItemPropertyValue 'HKCU:\software\microsoft\windows\currentversion\explorer\shell folders\' -Name '{374DE290-123F-4565-9164-39C4925E467B}'
 Add-MpPreference -ExclusionPath $Downloads
 Add-MpPreference -ExclusionPath "C:\payloads\"
-Add-MpPreference -ExclusionPath "C:\tools\"
+Add-MpPreference -ExclusionPath "$toolsPath\"
 Set-MpPreference -MAPSReporting Disabled
 Set-MpPreference -SubmitSamplesConsent NeverSend
 
@@ -35,7 +35,7 @@ choco install tabby
 choco install firefox
 choco install putty
 choco install vscode
-choco install sysinternals --params "/InstallDir:C:\tools\sysinternals"
+choco install sysinternals --params "/InstallDir:$toolsPath\sysinternals"
 # choco install visualstudio2019community -y
 choco install openvpn -y
 choco install powertoys -y
@@ -76,47 +76,47 @@ $gitPath = "C:\Program Files\Git\bin"
 $env:Path = "$env:Path;$gitPath"
 Add-Content -Path $PROFILE -Value "Set-Location '$gitPath'"
 
-# Git Tools
-Invoke-WebRequest -Uri https://github.com/dnSpy/dnSpy/releases/latest/download/dnSpy-netframework.zip -OutFile "$env:TEMP\dnSpy-netframework.zip"
-Expand-Archive -Path "$env:TEMP\dnSpy-netframework.zip" -DestinationPath C:\tools\dnSpy
+$toolPath = "C:\Tools" 
 
 # Need to refresh or reload here
-git clone https://github.com/BloodHoundAD/SharpHound3.git C:\tools\SharpHound3
-git clone https://github.com/dafthack/MailSniper.git C:\tools\MailSniper
-git clone https://github.com/decoder-it/juicy-potato.git C:\tools\juicy-potato
-git clone https://github.com/djhohnstein/SharpChrome.git C:\tools\SharpChrome
-git clone https://github.com/FortyNorthSecurity/Egress-Assess.git C:\tools\Egress-Assess
-git clone https://github.com/FSecureLABS/SharpGPOAbuse.git C:\tools\SharpGPOAbuse
-git clone https://github.com/gentilkiwi/mimikatz.git C:\tools\mimikatz
-git clone https://github.com/GhostPack/Seatbelt.git C:\tools\Seatbelt
-git clone https://github.com/HarmJ0y/DAMP.git C:\tools\DAMP
-git clone https://github.com/hfiref0x/UACME.git C:\tools\UACME
-git clone https://github.com/leechristensen/SpoolSample.git C:\tools\SpoolSample
+git clone https://github.com/BloodHoundAD/SharpHound3.git $toolsPath\SharpHound3
+git clone https://github.com/dafthack/MailSniper.git $toolsPath\MailSniper
+git clone https://github.com/decoder-it/juicy-potato.git $toolsPath\juicy-potato
+git clone https://github.com/djhohnstein/SharpChrome.git $toolsPath\SharpChrome
+git clone https://github.com/FortyNorthSecurity/Egress-Assess.git $toolsPath\Egress-Assess
+git clone https://github.com/FSecureLABS/SharpGPOAbuse.git $toolsPath\SharpGPOAbuse
+git clone https://github.com/gentilkiwi/mimikatz.git $toolsPath\mimikatz
+git clone https://github.com/GhostPack/Seatbelt.git $toolsPath\Seatbelt
+git clone https://github.com/HarmJ0y/DAMP.git $toolsPath\DAMP
+git clone https://github.com/hfiref0x/UACME.git $toolsPath\UACME
+git clone https://github.com/leechristensen/SpoolSample.git $toolsPath\SpoolSample
 git clone https://github.com/NetSPI/MicroBurst
-git clone https://github.com/NetSPI/PowerUpSQL.git C:\tools\PowerUpSQL
-git clone https://github.com/p3nt4/PowerShdll.git C:\tools\PowerShdll
-git clone https://github.com/PowerShellMafia/PowerSploit.git C:\tools\PowerSploit
-git clone https://github.com/rasta-mouse/MiscTools.git C:\tools\MiscTools
-git clone https://github.com/rasta-mouse/Sherlock.git C:\tools\Sherlock
-git clone https://github.com/rasta-mouse/Watson.git C:\tools\Watson
-git clone https://github.com/tevora-threat/SharpView.git C:\tools\SharpView
-git clone https://github.com/TheWover/donut.git C:\tools\donut
-git clone https://github.com/ZeroPointSecurity/PhishingTemplates.git C:\tools\PhishingTemplates
-git clone https://github.com/MelloSec/Sessioner
-git clone https://github.com/rvrsh3ll/TokenTactics.git
-git clone https://github.com/mgeeky/AzureRT
-git clone https://github.com/0xZDH/o365spray.git
-git clone https://github.com/knavesec/CredMaster.git
-git clone https://github.com/dafthack/GraphRunner.git
-git clone https://github.com/cyberark/blobhunter
-git clone https://github.com/csandker/Azure-AccessPermissions
-git clone https://github.com/hausec/PowerZure
+git clone https://github.com/NetSPI/PowerUpSQL.git $toolsPath\PowerUpSQL
+git clone https://github.com/p3nt4/PowerShdll.git $toolsPath\PowerShdll
+git clone https://github.com/PowerShellMafia/PowerSploit.git $toolsPath\PowerSploit
+git clone https://github.com/rasta-mouse/MiscTools.git $toolsPath\MiscTools
+git clone https://github.com/rasta-mouse/Sherlock.git $toolsPath\Sherlock
+git clone https://github.com/rasta-mouse/Watson.git $toolsPath\Watson
+git clone https://github.com/tevora-threat/SharpView.git $toolsPath\SharpView
+git clone https://github.com/TheWover/donut.git $toolsPath\donut
+git clone https://github.com/ZeroPointSecurity/PhishingTemplates.git $toolsPath\PhishingTemplates
+git clone https://github.com/rvrsh3ll/TokenTactics.git $toolsPath\TokenTactics
+git clone https://github.com/mgeeky/AzureRT $toolsPath\AzureRT
+git clone https://github.com/0xZDH/o365spray.git $toolsPath\o365spray
+git clone https://github.com/dafthack/GraphRunner.git $toolsPath\GraphRunner
+git clone https://github.com/csandker/Azure-AccessPermissions $toolsPath\Azure-AccessPermissions
+git clone https://github.com/hausec/PowerZure $toolsPath\PowerZure
+git clone https://github.com/MelloSec/Sessioner $toolsPath\Sessioner
+git clone https://github.com/MelloSec/PhirstPhish $toolsPath\PhirstPhish
 
-iwr https://github.com/BloodHoundAD/AzureHound/releases/download/v2.0.4/azurehound-linux-amd64.zip -o azurehound.zip
-Expand-Archive azurehound.zip
 
-iwr https://github.com/Flangvik/TeamFiltration/releases/download/v3.5.0/TeamFiltration-Linux-v3.5.0.zip -o teamfiltration.zip
-unzip ./teamfiltration.zip
+
+
+iwr https://github.com/BloodHoundAD/AzureHound/releases/download/v2.0.4/azurehound-linux-amd64.zip -o $toolsPath\azurehound.zip
+Expand-Archive $toolsPath\azurehound.zip
+
+iwr https://github.com/Flangvik/TeamFiltration/releases/download/v3.5.0/TeamFiltration-Linux-v3.5.0.zip -o $toolsPath\teamfiltration.zip
+Expand-Archive $toolsPath\teamfiltration.zip
 
 # IE first run
 New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer"
@@ -125,11 +125,11 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Internet Explorer\Main
 
 # BloodHound
 Invoke-WebRequest -Uri 'https://github.com/BloodHoundAD/BloodHound/releases/latest/download/BloodHound-win32-x64.zip' -OutFile "$env:TEMP\BloodHound.zip"
-Expand-Archive -Path "$env:TEMP\BloodHound.zip" -DestinationPath C:\tools\
-Rename-Item -Path C:\tools\BloodHound-win32-x64\ -NewName BloodHound
+Expand-Archive -Path "$env:TEMP\BloodHound.zip" -DestinationPath $toolsPath\
+Rename-Item -Path $toolsPath\BloodHound-win32-x64\ -NewName BloodHound
 Invoke-WebRequest -Uri 'https://neo4j.com/artifact.php?name=neo4j-community-4.0.0-windows.zip' -OutFile "$env:TEMP\neo4j.zip"
-Expand-Archive -Path "$env:TEMP\neo4j.zip" -DestinationPath C:\tools\
-Rename-Item -Path C:\tools\neo4j-community-4.0.0\ -NewName Neo4j
+Expand-Archive -Path "$env:TEMP\neo4j.zip" -DestinationPath $toolsPath\
+Rename-Item -Path $toolsPath\neo4j-community-4.0.0\ -NewName Neo4j
 
 ## Visual Studio
 Invoke-WebRequest -Uri 'https://visualstudioclient.gallerycdn.vsassets.io/extensions/visualstudioclient/microsoftvisualstudio2017installerprojects/1.0.0/1620063166533/InstallerProjects.vsix' -OutFile "$Downloads\InstallerProjects.vsix"
@@ -163,8 +163,8 @@ c:\sysmon\sysmon.exe -acceptEula -i c:\sysmon\sysmon-swift.xml
 # UI
 Import-Module "$env:ChocolateyInstall\helpers\chocolateyInstaller.psm1" -Force
 Set-WindowsExplorerOptions -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableExpandToOpenFolder -EnableShowRibbon
-Install-ChocolateyShortcut -shortcutFilePath "C:\Users\Public\Desktop\tools.lnk" -targetPath C:\tools\
-Install-ChocolateyShortcut -shortcutFilePath "C:\Users\Public\Desktop\Neo4j.lnk" -targetPath "C:\tools\Neo4j\bin\neo4j.bat" -arguments "console" -runAsAdmin
+Install-ChocolateyShortcut -shortcutFilePath "C:\Users\Public\Desktop\tools.lnk" -targetPath $toolsPath\
+Install-ChocolateyShortcut -shortcutFilePath "C:\Users\Public\Desktop\Neo4j.lnk" -targetPath "$toolsPath\Neo4j\bin\neo4j.bat" -arguments "console" -runAsAdmin
 
 # Misc
 $DesktopPath = [Environment]::GetFolderPath("Desktop")
@@ -194,29 +194,36 @@ Import-Module AADInternals
 Import-Module Microsoft.Graph
 
 # Token Tactics
-git clone https://github.com/rvrsh3ll/TokenTactics.git
-cd TokenTactics
-Import-Module C:\Git\Phishing\O365\TokenTactics\TokenTactics.psd1
+git clone https://github.com/rvrsh3ll/TokenTactics.git $toolsPath\TokenTactics
+cd $toolsPath\TokenTactics
+Import-Module .\TokenTactics.psd1
 cd ..
 
 # Password Spraying Tools
 # https://github.com/knavesec/CredMaster/wiki/
-git clone https://github.com/knavesec/CredMaster.git
-cd CredMaster
+git clone https://github.com/knavesec/CredMaster.git $toolsPath\CredMaster
+cd $toolsPath\CredMaster
 pip install -r requirements.txt
 cd ..
 
+# BlobHunter
+git clone https://github.com/cyberark/BlobHunter.git $toolsPath\BlobHunter
+cd $toolsPath\Blobhunter
+pip3 install -r requirements.txt
+
+# Road Tools
 pip install roadlib
 pip install roadrecon
 pip install roadtx
+
+# TrevorSpray
 pip install git+https://github.com/blacklanternsecurity/trevorproxy
 pip install git+https://github.com/blacklanternsecurity/trevorspray
-
 
 # Set a nice wallpaper : 
 write-host "Setting a nice wallpaper"
 $web_dl = new-object System.Net.WebClient
-$wallpaper_url = "http://tessier-ashpool.s3.amazonaws.com/computerdesktop.jpg"
+$wallpaper_url = "https://dev.straylightsecurity.com/assets/lich3.jpg"
 $wallpaper_file = "C:\Users\Public\Pictures\desktop.jpg"
 $web_dl.DownloadFile($wallpaper_url, $wallpaper_file)
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\Users\Public\Pictures\desktop.jpg" /f
@@ -224,12 +231,8 @@ reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v WallpaperStyle /t REG_DWORD
 reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v StretchWallpaper /t REG_DWORD /d "2" /f 
 reg add "HKEY_CURRENT_USER\Control Panel\Colors" /v Background /t REG_SZ /d "0 0 0" /f
 
-
-
-
-
 Enable-PSRemoting -Force
-Update-Help
+# Update-Help
 Enable-WindowsUpdate
 Enable-RemoteDesktop
 Install-WindowsUpdate

1b0dac77da2418a67f95764499d3f3226c0c4b46
mellonaut <rleeazw@outlook.com>
2023-12-16 16:17:16 -0500

    working on Cloud

--- Cloud.ps1
+++ Cloud.ps1
@@ -1,34 +1,8 @@
-whoami
+# Install PowerShell Modules with force and auto accept
+$modules = @("AADInternals", "AzureADPreview", "Az", "AzureAd", "Microsoft.Graph")
+foreach ($module in $modules) {
+    Install-Module -Name $module -Force -AllowClobber -Scope CurrentUser -Confirm:$false
+}
 
-# # Install PowerShell Modules with force and auto accept
-# $modules = @("AADInternals", "AzureADPreview", "Az", "AzureAd", "Microsoft.Graph")
-# foreach ($module in $modules) {
-#     Install-Module -Name $module -Force -AllowClobber -Scope CurrentUser -Confirm:$false
-# }
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# # # Function to check if Chocolatey is already installed
-# # function IsChocolateyInstalled {
-# #     $chocoPath = Get-Command choco -ErrorAction SilentlyContinue
-# #     return $chocoPath -ne $null
-# # }
-
-# # # Install Chocolatey if it's not already installed
-# # if (-not (IsChocolateyInstalled)) {
-# #     # Set up the Chocolatey package manager
-# #     [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
-# #     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-# # }
-# # else {
-# #     Write-Output "Chocolatey is already installed."
-# # }
-
-# # Verify installations
-# Write-Output "Installed PowerShell Modules:"
-# Get-Module -ListAvailable $modules
-
-# # # Check if Chocolatey is installed
-# # if (IsChocolateyInstalled) {
-# #     Write-Output "Chocolatey is installed."
-# # } else {
-# #     Write-Output "Chocolatey installation failed."
-# # }

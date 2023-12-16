6209c7e9f825ed3604cdf072d7d8e954224fab32
mellonaut <rleeazw@outlook.com>
2023-12-16 16:42:38 -0500

    working on Cloud

--- Cloud.ps1
+++ Cloud.ps1
@@ -1,8 +1,10 @@
-# Install PowerShell Modules with force and auto accept
-$modules = @("AADInternals", "AzureADPreview", "Az", "AzureAd", "Microsoft.Graph")
-foreach ($module in $modules) {
-    Install-Module -Name $module -Force -AllowClobber -Scope CurrentUser -Confirm:$false
-}
+whoami
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Install PowerShell Modules with force and auto accept
+# $modules = @("AADInternals", "AzureADPreview", "Az", "AzureAd", "Microsoft.Graph")
+# foreach ($module in $modules) {
+#     Install-Module -Name $module -Force -AllowClobber -Scope CurrentUser -Confirm:$false
+# }
+
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 

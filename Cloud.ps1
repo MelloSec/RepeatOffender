03b6cbfb61e9e2648ead9d0417e515988f26eb92
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-12-17 16:52:40 -0500

    Update Cloud.ps1

--- Cloud.ps1
+++ Cloud.ps1
@@ -6,5 +6,5 @@ whoami
 #     Install-Module -Name $module -Force -AllowClobber -Scope CurrentUser -Confirm:$false
 # }
 
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 

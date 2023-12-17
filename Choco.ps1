a0d3a72cb716378147f482fbb3867121b6ed9f18
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-12-17 16:53:25 -0500

    Create Choco.ps1

---
+++ Choco.ps1
@@ -0,0 +1,7 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+# Install PowerShell Modules with force and auto accept
+# $modules = @("AADInternals", "AzureADPreview", "Az", "AzureAd", "Microsoft.Graph")
+# foreach ($module in $modules) {
+#     Install-Module -Name $module -Force -AllowClobber -Scope CurrentUser -Confirm:$false
+# }

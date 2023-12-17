e8f823b9379c0dfa24625a94adc452aff6a7888b
mellonaut <65114647+mellonaut@users.noreply.github.com>
2023-12-17 17:28:23 -0500

    Update CloudBig.ps1

--- CloudBig.ps1
+++ CloudBig.ps1
@@ -1,6 +1,6 @@
 # Install boxstarter with chocolatey and basic configuration
-. { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+# . { iwr -useb https://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
+# Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 Set-TimeZone -Name "Eastern Standard Time" -Verbose
 
 New-Item -Path C:\ -Name Temp -ItemType Directory -ErrorAction SilentlyContinue

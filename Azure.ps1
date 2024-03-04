ebf5d9d15d55cca0316fe74f3fa23d7770b7fa34
mellonaut <65114647+mellonaut@users.noreply.github.com>
2024-03-04 18:42:10 -0500

    Update Azure.ps1

--- Azure.ps1
+++ Azure.ps1
@@ -3,6 +3,7 @@ choco install -y azure-cli
 choco install -y git
 
 # Powershell Modules
+# Install-Module PSWindowsUpdate -Accept -Force
 Install-Module aadinternals -force  -AllowClobber -Scope CurrentUser
 Install-Module Az -Force -AllowClobber -Scope CurrentUser
 Install-Module -Name ExchangeOnlineManagement -Force  -AllowClobber -Scope CurrentUser
@@ -50,9 +51,6 @@ git clone https://github.com/dafthack/MailSniper.git
 # MFASweep check for 2FA bypasses
 git clone https://github.com/dafthack/MFASweep
 
-# MSOLSpray for fine-enumeration of accounts / checking valid creds without a PUSH notification
-git clone https://github.com/dafthack/MSOLSpray
-
 # O365Creeper
 git clone https://github.com/LMGsec/o365creeper
 
@@ -61,6 +59,18 @@ git clone https://github.com/AlteredSecurity/365-Stealer.git
 
 # TokenTactics
 git clone https://github.com/rvrsh3ll/TokenTactics.git
+
+# TokenTactics V2 - CAE Tokens
+git clone https://github.com/f-bader/TokenTacticsV2.git
+
+# TokenTormentor
+git clone https://github.com/CompassSecurity/TokenTormentor
+
+# TokenPhisher
+git clone https://github.com/CompassSecurity/TokenPhisher
+
+# PhirstPhish
+git clone https://github.com/mellosec/phirstphish
 
 # AzureAppTools
 git clone https://github.com/rvrsh3ll/Azure-App-Tools
@@ -75,11 +85,28 @@ git clone https://github.com/NetSPI/MicroBurst.git
 git clone https://github.com/hausec/PowerZure.git
 
 # Egress Assessor
-git clone https://github.com/FortyNorthSecurity/Egress-Assess.git $toolsPath\Egress-Assess
+git clone https://github.com/FortyNorthSecurity/Egress-Assess.git
+
+# MSOLSpray for fine-enumeration of accounts / checking valid creds without a PUSH notification
+git clone https://github.com/dafthack/MSOLSpray
+
+# Omnispray - Spiritual Succesor of MSOLspray
+git clone https://github.com/0xZDH/Omnispray.git
+
+# o365spray - Re-Implement spraying in Go
+git clone https://github.com/0xZDH/o365spray
+
+# go365 - spray RST endpoint
+git clone https://github.com/optiv/Go365
+
+# Spray365
+git clone https://github.com/MarkoH17/Spray365
+cd Spray365
+pip install -r requirements.txt -U
+python spray365.py
+cd ..
 
 
-
-### Python Based
 # BlobHunter
 git clone https://github.com/cyberark/BlobHunter.git $toolsPath\BlobHunter
 cd $toolsPath\Blobhunter
@@ -89,8 +116,21 @@ cd ..
 # ROADTools
 git clone https://github.com/dirkjanm/ROADtools.git
 pip install roadrecon roadlib roadtx neo4j-driver
-iwr https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-win-aarch64.zip -o geckodriver-win.zip
-# iwr https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux-aarch64.tar.gz -o geckodriver-lin.tar.gz
+
+# Check Operating System and Download Correct geckodriver Version
+if ($IsWindows) {
+    # For Windows
+    iwr https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-win-aarch64.zip -Outfile geckodriver-win.zip
+    Expand-Archive -Path geckodriver-win.zip -DestinationPath .\geckodriver
+    Remove-Item geckodriver-win.zip # Clean up ZIP file
+} elseif ($IsLinux) {
+    # For Linux
+    iwr https://github.com/mozilla/geckodriver/releases/download/v0.33.0/geckodriver-v0.33.0-linux-aarch64.tar.gz -Outfile geckodriver-lin.tar.gz
+    tar -xzf geckodriver-lin.tar.gz -C ./geckodriver
+    Remove-Item geckodriver-lin.tar.gz # Clean up TAR.GZ file
+} else {
+    Write-Output "Unsupported OS."
+}
 
 # Fireprox
 git clone https://github.com/ustayready/fireprox
@@ -103,13 +143,6 @@ cd ..
 
 # CAP Gap Analyzer
 git clone https://github.com/jsa2/caOptics
-
-# Spray365
-git clone https://github.com/MarkoH17/Spray365
-cd Spray365
-pip install -r requirements.txt -U
-python spray365.py
-cd ..
 
 # Bridgekeeper for scraping linkedin
 git clone https://github.com/0xZDH/BridgeKeeper
